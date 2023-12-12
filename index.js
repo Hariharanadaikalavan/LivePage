9a64642047a64f2ed5896b1058d00a1c159c505f
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-12 23:17:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var username=prompt("Enter your Name Respectively");
 
-var output=alert("Greeting!!! Welcome To My Page Mr."+username+" "+"Enjoy Ours Services-Eppadiku HariharanRamanathan");
+var output=alert("Greeting!!! Welcome To My Page Mr."+username+" "+"Enjoy Ours Services-Eppadiku HariharanRamanathan B.E-Mechanical ");
