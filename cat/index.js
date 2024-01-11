1b697a74bfa5c0ec323aa381ef79536fa834c2cf
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2024-01-11 22:18:27 +0530

    Update index.js

--- cat/index.js
+++ cat/index.js
@@ -1,4 +1,4 @@
-function lighton(){
+function lighton(){  
     document.getElementById("light").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
     document.getElementById("eye").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
     document.getElementById("onbtn").style.background="Green";
